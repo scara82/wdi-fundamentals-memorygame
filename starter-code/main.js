deef72d8fa5b82cc57a2a4c79047dcf9acbfb6df
scara82 <marta.scaramella@gmail.com>
2017-01-30 10:00:21 +1100

    pop up

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,13 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen":
+var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne === cardFour) {
+	alert("You found a match!");
+}
+else{
+	alert("Sorry, try again.");
+}
