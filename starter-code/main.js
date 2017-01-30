7fd3e5d5775341b705bbfb31aad7ef460f74b00d
scara82 <marta.scaramella@gmail.com>
2017-01-31 10:17:02 +1100

    new cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -11,3 +11,22 @@ if (cardOne === cardFour) {
 else{
 	alert("Sorry, try again.");
 }
+
+var boardBuilder = document.getElementById('game-board');
+
+var createCards = function(){
+	var i = function() {
+for (var i=0; i<4; i++) {
+	var newCard = document.createElement('div');
+	newCard.className = "card";
+	newCard.textContent = "";
+	boardBuilder.appendChild(newCard);
+}
+}
+}
+
+createCards();
+
+
+
+
